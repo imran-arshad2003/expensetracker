c8651a74c9d0b2974cae8b8a431b58c097442978
imran-arshad2003 <imran.arshad.waada@gmail.com>
2026-01-28 13:37:30 +0500

    add delete history buton

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,13 +1,27 @@
 function History() {
+  const expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+  const clearAll = () => {
+    if (confirm("Are you sure to delete it?")) {
+      localStorage.removeItem("expenses");
+      window.location.reload();
+    }
+  };
+
   return (
     <div>
       <h1 className="font-bold">History</h1>
-      {JSON.parse(localStorage.getItem("expenses")) && JSON.parse(localStorage.getItem("expenses")).reverse().map((expenses,index)=>(
+
+      <button onClick={clearAll}>
+        Clear all
+      </button>
+
+      {[...expenses].reverse().map((expense, index) => (
         <div key={index} className="border-b border-gray-300 py-2">
-          <p>Price: {expenses.price}</p>
-          <p>Description: {expenses.description}</p>
-          <p>Type: {expenses.type}</p>
-          <p>Date: {new Date(expenses.date).toLocaleString("en-US")}</p>
+          <p>Price: {expense.price}</p>
+          <p>Description: {expense.description}</p>
+          <p>Type: {expense.type}</p>
+          <p>Date: {new Date(expense.date).toLocaleString("en-US")}</p>
         </div>
       ))}
     </div>
